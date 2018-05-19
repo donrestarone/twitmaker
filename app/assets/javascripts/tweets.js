ac8e86c25266e0e21eb0c90863b5927233e5dc2d
donrestarone <shashikejayatunge@gmail.com>
2018-05-19 11:40:48 -0400

    need a lot more work to be usable. api call working, yet to figure out how to add date time to the ajax call

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,38 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+document.addEventListener("DOMContentLoaded", function(event){
+	var form = document.getElementById('new_tweet');
+
+	form.addEventListener('submit', function(event){
+		event.preventDefault();
+		
+		$.ajax({
+			url: form.getAttribute('action'),
+			method: form.getAttribute('method'),
+			dataType: 'json',
+			data: $(form).serialize(),
+		}).done(function(data){
+			console.log(data.tweet);
+			var tweetText = (data.tweet);
+			var tweetList = document.querySelector('.tweets');
+
+			var tweet = document.createElement('li');
+			var tweetPTag = document.createElement('p');
+			tweetPTag.innerText = tweetText;
+			//tweetTime.innerText = (data.time);
+			var tweetTime = document.createElement('time');
+
+			tweet.append(tweetPTag, tweetTime);
+			$(tweetList).append(tweet);
+
+			console.log('ajax call successful');
+		}).fail(function(jqXHR, textStatus, errorThrown){
+			console.log(jqXHR + textStatus + errorThrown);
+		})
+	});
+
+
+
+});
+
+
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,7 +8,15 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new(tweet_params)
 
     if @tweet.save
-      redirect_to tweets_path
+      #redirect_to tweets_path
+      if request.xhr?
+        respond_to do |form|
+          form.html do
+            render :index
+          end 
+          form.json {render json: {tweet: @tweet.message, time: @tweet.created_at.strftime('%b %e, %l:%M %p').to_s}}
+        end
+      end
     else
       render :index
     end
